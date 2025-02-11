135557c80556878645fd2415421fee6add5d6f87
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-11 12:07:14 +0530

    Add priority label and date to task list

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,14 +1,19 @@
 const TaskList = () => {
   return (
     <div id="tasklist" className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10">
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+          <h4>11 feb 2025</h4>
+        </div>
+      </div>
+      {/* <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
       <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
       <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
       <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
       <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
       <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div> */}
     </div>
   );
 }
