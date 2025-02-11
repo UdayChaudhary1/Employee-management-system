a8948d689ad5bb05fa51ed6143a7e614fe47e491
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-11 12:26:53 +0530

    Add form to admin dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -4,7 +4,23 @@ const AdminDashboard = () => {
   return (
     <div className="h-screen w-full p-10">
       <Header />
+      <div>
+        <form>
+          <h3>Task Title</h3>
+          <input type="text" placeholder="Make UI design" />
+          <h3>Description</h3>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <h3>Date</h3>
+          <input type="date" />
+          <h3>Assign to</h3>
+          <input type="text" placeholder="employee name" />
+          <h3>Category</h3>
+          <input type="text" placeholder="design, dev, etc" />
+          <button>Create Task</button>
+        </form>
+      </div>
     </div>
+    
   )
 }
 export default AdminDashboard
