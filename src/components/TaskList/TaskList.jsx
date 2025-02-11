be6c4bd402c22c0f9fb9e52c2edf668fe8f77383
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-11 12:13:58 +0530

    Add title and description to the task

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,20 +1,55 @@
 const TaskList = () => {
   return (
-    <div id="tasklist" className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10">
+    <div
+      id="tasklist"
+      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10"
+    >
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
         <div className="flex justify-between items-center">
-          <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
-          <h4>11 feb 2025</h4>
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">11 feb 2025</h4>
         </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete the project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          aliquid placeat necessitatibus illo id sunt?
+        </p>
       </div>
-      {/* <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div> */}
+
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">11 feb 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete the project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          aliquid placeat necessitatibus illo id sunt?
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">11 feb 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete the project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          aliquid placeat necessitatibus illo id sunt?
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">11 feb 2025</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Complete the project</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          aliquid placeat necessitatibus illo id sunt?
+        </p>
+      </div>
     </div>
   );
-}
-export default TaskList
+};
+export default TaskList;
