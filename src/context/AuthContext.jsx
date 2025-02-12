591b5ac4f90e93a6183d4a5d4b477dfbc7b2bc8d
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-12 12:59:13 +0530

    Create TaskContext and AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+
+const AuthContext = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+AuthContext.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default AuthContext;
