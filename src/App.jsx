0f5320d7a3f551bd93af2fdb789ba9ef047469d6
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-16 15:56:42 +0530

    Create handleLogin function
    handleLogin function can handle login
    functionalities. It can also distinguish between
    admin and employee.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-// import Login from "./components/Auth/Login";
-
-import AdminDashboard from "./components/Dashboard/AdminDashboard";
-// import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <>
-      {/* <Login /> */}
-      {/* <EmployeeDashboard /> */}
-      <AdminDashboard />
-    </>
-  );
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser("admin");
+      console.log(user);
+    } else if (email == "user@me.com" && password == "123") {
+      setUser("employee");
+      console.log(user);
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
+  return <></>;
 };
 export default App;
