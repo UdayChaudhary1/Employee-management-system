c777aa1b8414e319f8276d9b9570747f3bcbf217
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-16 16:07:47 +0530

    Display appropriate dashboard
    if user is admin then display AdminDashboard
    otherwise display EmployeeDashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Login from "./components/Auth/Login";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -16,6 +18,11 @@ const App = () => {
     }
   };
 
-  return <>{!user ? <Login handleLogin={handleLogin} /> : ""}</>;
+  return (
+    <>
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
+    </>
+  );
 };
 export default App;
