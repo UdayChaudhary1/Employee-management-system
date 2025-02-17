cb623f8a221d75125ab40d41ec3e8c53fabb80bc
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-17 15:25:23 +0530

    Create logic to persist user

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+      if (loggedInUser) {
+        setUser(loggedInUser.role);
+      }
+    }
+  }, [authData]);
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
-      console.log(user);
-    } else if (email == "user@me.com" && password == "123") {
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (
+      authData &&
+      authData.employees.find((e) => email == e.email && e.password == password)
+    ) {
       setUser("employee");
-      console.log(user);
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({ role: "employee" })
+      );
     } else {
       alert("Invalid credentials");
     }
